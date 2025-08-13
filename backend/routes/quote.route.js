1ddc2b61507b88200a9c6f3e380f28e2826ea875
shlokarth911 <shlokarth011@gmail.com>
2025-08-14 02:55:12 +0530

    Initial project setup with package.json and server.js files

--- backend/routes/quote.route.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/quotes.js
-const express = require("express");
-const router = express.Router();
-const { quickQuote } = require("../controllers/quote.controller.js");
-
-// POST /api/venues/:id/quote
-router.post("/venues/:id/quote", quickQuote);
-
-module.exports = router;
