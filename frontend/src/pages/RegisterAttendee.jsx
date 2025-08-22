ffb66b26b600976428538b551fb1d161ba86c034
shlokarth911 <shlokarth011@gmail.com>
2025-08-23 01:55:37 +0530

    Enhance attendee registration form with state management and submit handler

--- frontend/src/pages/RegisterAttendee.jsx
+++ frontend/src/pages/RegisterAttendee.jsx
@@ -1,8 +1,36 @@
-import React from "react";
-import { ArrowLeft } from "lucide-react";
+import React, { useState } from "react";
+import { ArrowLeft, EqualApproximately } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const RegisterAttendee = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [city, setCity] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const attendeeData = {
+      name: name,
+      email: email,
+      password: password,
+      city: city,
+    };
+
+    console.log(attendeeData);
+  };
+
+  const cities = [
+    {
+      name: "Ranchi",
+      value: "Ranchi",
+    },
+    {
+      name: "Delhi",
+      value: "Delhi",
+    },
+  ];
+
   return (
     <div className="bg-neutral-900 h-screen text-white">
       <div className=" px-5 py-4 flex items-center justify-between">
@@ -16,28 +44,42 @@ const RegisterAttendee = () => {
       <div className="fixed bottom-0 left-0 right-0">
         <div className="px-5 py-6">
           <h1 className="text-3xl">
-            Register as an <br /> Attendee
+            Register as an <br /> Attendee / Host
           </h1>
+
+          <p className="pt-2 text-neutral-300">Book , host or visit events</p>
         </div>
         <div className="bg-neutral-800 px-5 pt-9  pb-7 rounded-t-4xl">
-          <form>
+          <form onSubmit={submitHandler}>
             <h2 className="text-xl  ml-4 text-neutral-300">Name</h2>
             <input
               type="text"
               placeholder="Enter your name"
               className="bg-neutral-700 text-lg px-5 py-4 mt-2 w-full rounded-full"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              value={name}
             />
             <h2 className="text-xl mt-6 ml-4 text-neutral-300">Email</h2>
             <input
               type="email"
               placeholder="Enter your email"
               className="bg-neutral-700 text-lg px-5 py-4 mt-2 w-full rounded-full"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
             />
             <h2 className="text-xl mt-6 ml-4 text-neutral-300">Passowrd</h2>
             <input
               type="password"
               placeholder="Enter a strong password"
               className="bg-neutral-700 text-lg px-5 py-4 mt-2 w-full rounded-full"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
             />
             <h2 className="text-xl mt-6 ml-4 text-neutral-300">
               City (Optional)
@@ -45,13 +87,16 @@ const RegisterAttendee = () => {
             <select
               placeholder="Click to select your city"
               className="bg-neutral-700 text-lg px-5 py-4 mt-2 w-full rounded-full appearance-none"
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
             >
-              <option className="" value="">
-                Ranchi
-              </option>
-              <option className="" value="">
-                Delhi
-              </option>
+              {cities.map((e, idx) => {
+                return (
+                  <option key={idx} value={e.value} className="">
+                    {e.name}
+                  </option>
+                );
+              })}
             </select>
             <button
               className="w-full mt-9 text-2xl text-black py-3 rounded-full"
@@ -70,5 +115,3 @@ const RegisterAttendee = () => {
 };
 
 export default RegisterAttendee;
-
-//do e.preventDefault
